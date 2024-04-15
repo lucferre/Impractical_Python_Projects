4d275088185cd71415c5235e0c6a0dbe0eb6bd31
Lucas Ferreyra <ferreyralucas@outlook.com>
2024-04-15 11:24:28 +0200

    encapsulate code in main function

--- 1_Silly_Name_Generator/pseudonyms.py
+++ 1_Silly_Name_Generator/pseudonyms.py
@@ -1,60 +1,66 @@
 import sys, random
 
-print("Welcome to the Psych 'Sidekick Name Picker.'\n")
-print("A name just like Sean would pick for Gus:\n\n")
+def main():
+    print("Welcome to the Psych 'Sidekick Name Picker.'\n")
+    print("A name just like Sean would pick for Gus:\n\n")
 
-#Load a list of first names
-first = ('Baby Oil', 'Bad News', 'Big Burps', "Bill 'Beenie-Weenie'",
-         "Bob 'Stinkbug'", 'Bowel Noises', 'Boxelder', "Bud 'Lite'",
-         'Butterbean', 'Buttermilk', 'Buttocks', 'Chad', 'Chesterfield',
-         'Chewy', 'Chigger', 'Cinnabuns', 'Cleet', 'Cornbread', 'Crab Meat',
-         'Crapps', 'Dark Skies', 'Dennis Clawhammer', 'Dicman', 'Elphonso',
-         'Fancypants', 'Figgs', 'Foncy', 'Gootsy', 'Greasy Jim', 'Huckleberry',
-         'Huggy', 'Ignatious', 'Jimbo', "Joe 'Pottin Soil'", 'Johnny',
-         'Lemongrass', 'Lil Debil', 'Longbranch', '"Lunch Money"', 'Mergatroid',
-         '"Mr Peabody"', 'Oil-Can', 'Oinks', 'Old Scratch', 'Ovaltine',
-         'Pennywhistle', 'Pitchfork Ben', 'Potato Bug', 'Pushmeet',
-         'Rock Candy', 'Schlomo', 'Scratchensniff', 'Scut',
-         "Sid 'The Squirts'", 'Skidmark', 'Slaps', 'Snakes', 'Snoobs',
-         'Snorki', 'Soupcan Sam', 'Spitzitout', 'Squids', 'Stinky',
-         'Storyboard', 'Sweet Tea', 'TeeTee', 'Wheezy Joe',
-         "Winston 'Jazz Hands'", 'Worms')
+    #Load a list of first names
+    first = ('Baby Oil', 'Bad News', 'Big Burps', "Bill 'Beenie-Weenie'",
+            "Bob 'Stinkbug'", 'Bowel Noises', 'Boxelder', "Bud 'Lite'",
+            'Butterbean', 'Buttermilk', 'Buttocks', 'Chad', 'Chesterfield',
+            'Chewy', 'Chigger', 'Cinnabuns', 'Cleet', 'Cornbread', 'Crab Meat',
+            'Crapps', 'Dark Skies', 'Dennis Clawhammer', 'Dicman', 'Elphonso',
+            'Fancypants', 'Figgs', 'Foncy', 'Gootsy', 'Greasy Jim', 'Huckleberry',
+            'Huggy', 'Ignatious', 'Jimbo', "Joe 'Pottin Soil'", 'Johnny',
+            'Lemongrass', 'Lil Debil', 'Longbranch', '"Lunch Money"', 'Mergatroid',
+            '"Mr Peabody"', 'Oil-Can', 'Oinks', 'Old Scratch', 'Ovaltine',
+            'Pennywhistle', 'Pitchfork Ben', 'Potato Bug', 'Pushmeet',
+            'Rock Candy', 'Schlomo', 'Scratchensniff', 'Scut',
+            "Sid 'The Squirts'", 'Skidmark', 'Slaps', 'Snakes', 'Snoobs',
+            'Snorki', 'Soupcan Sam', 'Spitzitout', 'Squids', 'Stinky',
+            'Storyboard', 'Sweet Tea', 'TeeTee', 'Wheezy Joe',
+            "Winston 'Jazz Hands'", 'Worms')
 
-#Load a list of surnames
-last = ('Appleyard', 'Bigmeat', 'Bloominshine', 'Boogerbottom',
-        'Breedslovetrout', 'Butterbaugh', 'Clovenhoof', 'Clutterbuck',
-        'Cocktoasten', 'Endicott', 'Fewhairs', 'Gooberdapple', 'Goodensmith',
-        'Goodpasture', 'Guster', 'Henderson', 'Hooperbag', 'Hoosenater',
-        'Hootkins', 'Jefferson', 'Jenkins', 'Jingley-Schmidt', 'Johnson',
-        'Kingfish', 'Listenbee', "M'Bembo", 'McFadden', 'Moonshine', 'Nettles',
-        'Noseworthy', 'Olivetti', 'Outerbridge', 'Overpeck', 'Overturf',
-        'Oxhandler', 'Pealike', 'Pennywhistle', 'Peterson', 'Pieplow',
-        'Pinkerton', 'Porkins', 'Putney', 'Quakenbush', 'Rainwater',
-        'Rosenthal', 'Rubbins', 'Sackrider', 'Snuggleshine', 'Splern',
-        'Stevens', 'Stroganoff', 'Sugar-Gold', 'Swackhamer', 'Tippins',
-        'Turnipseed', 'Vinaigrette', 'Walkingstick', 'Wallbanger', 'Weewax',
-        'Weiners', 'Whipkey', 'Wigglesworth', 'Wimplesnatch', 'Winterkorn',
-        'Woolysocks')
+    #Load a list of surnames
+    last = ('Appleyard', 'Bigmeat', 'Bloominshine', 'Boogerbottom',
+            'Breedslovetrout', 'Butterbaugh', 'Clovenhoof', 'Clutterbuck',
+            'Cocktoasten', 'Endicott', 'Fewhairs', 'Gooberdapple', 'Goodensmith',
+            'Goodpasture', 'Guster', 'Henderson', 'Hooperbag', 'Hoosenater',
+            'Hootkins', 'Jefferson', 'Jenkins', 'Jingley-Schmidt', 'Johnson',
+            'Kingfish', 'Listenbee', "M'Bembo", 'McFadden', 'Moonshine', 'Nettles',
+            'Noseworthy', 'Olivetti', 'Outerbridge', 'Overpeck', 'Overturf',
+            'Oxhandler', 'Pealike', 'Pennywhistle', 'Peterson', 'Pieplow',
+            'Pinkerton', 'Porkins', 'Putney', 'Quakenbush', 'Rainwater',
+            'Rosenthal', 'Rubbins', 'Sackrider', 'Snuggleshine', 'Splern',
+            'Stevens', 'Stroganoff', 'Sugar-Gold', 'Swackhamer', 'Tippins',
+            'Turnipseed', 'Vinaigrette', 'Walkingstick', 'Wallbanger', 'Weewax',
+            'Weiners', 'Whipkey', 'Wigglesworth', 'Wimplesnatch', 'Winterkorn',
+            'Woolysocks')
 
-while True:
-    #Choose a first name at random
-    #Assign the name to a variable
-    firstName = random.choice(first)
+    while True:
+        #Choose a first name at random
+        #Assign the name to a variable
+        first_name = random.choice(first)
 
-    #Choose a surname at random
-    #Assign the name to a variable
-    lastName = random.choice(last)
+        #Choose a surname at random
+        #Assign the name to a variable
+        last_name = random.choice(last)
 
-    #Print the names to the screen in order and in red font
-    print("\n\n")
-    print("{} {}".format(firstName, lastName), file=sys.stderr)
-    print("\n\n")
+        #Print the names to the screen in order and in red font
+        print("\n\n")
+        print("{} {}".format(first_name, last_name), file=sys.stderr)
+        print("\n\n")
 
-    #Ask the user to quit or play again
-    try_again = input("\n\nTry again? (Press Enter else n to quit)\n ")
-    #If user plays again:
-        #repeat
-    #If user quits:
-    if try_again.lower() == "n":
-        #end and exit
-        break
+        #Ask the user to quit or play again
+        try_again = input("\n\nTry again? (Press Enter else n to quit)\n ")
+        #If user plays again:
+            #repeat
+        #If user quits:
+        if try_again.lower() == "n":
+            #end and exit
+            break
+
+    input("\nPress Enter to exit.")
+
+if __name__ == "__main__":
+    main()
