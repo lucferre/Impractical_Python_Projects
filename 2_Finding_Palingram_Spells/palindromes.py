8db55a21d1b5ea06ef9f3ca56b8c8650808dab78
Lucas Ferreyra <ferreyralucas@outlook.com>
2024-04-16 11:30:51 +0200

    add code to palindromes script

--- 2_Finding_Palingram_Spells/palindromes.py
+++ 2_Finding_Palingram_Spells/palindromes.py
@@ -1,9 +1,19 @@
+"""Find palindromes (letter palingrams) in a dictionary file."""
+import load_dictionary
 # Assign empty list to a variable to store palindromes
+pali_list = []
 # Load dictionary file
+word_list = load_dictionary.load('words.txt')
+
 # Loop through each word in the file
-# Assign the word to a variable
+for word in word_list:
 # Assign the reversed word to a varible
-# Compare the two variables
-    # if they are equal:
-        # append the word to a list of palindromes
+    reversed_word = word[::-1]
+# If word and reversed word are equal:
+    if word == reversed_word:
+    # append the word to a list of palindromes
+        pali_list.append(word)
+        
 # Print to screen the array of palindromes
+print(f"Number of palindromes found = {len(pali_list)}\n")
+print(*pali_list, sep='\n')
