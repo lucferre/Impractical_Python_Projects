9db022c444db000cc96557d37610df4374ec7d29
Lucas Ferreyra <ferreyralucas@outlook.com>
2024-04-16 11:22:36 +0200

    create palindromes script and add pseudocode

---
+++ 2_Finding_Palingram_Spells/palindromes.py
@@ -0,0 +1,9 @@
+# Assign empty list to a variable to store palindromes
+# Load dictionary file
+# Loop through each word in the file
+# Assign the word to a variable
+# Assign the reversed word to a varible
+# Compare the two variables
+    # if they are equal:
+        # append the word to a list of palindromes
+# Print to screen the array of palindromes
